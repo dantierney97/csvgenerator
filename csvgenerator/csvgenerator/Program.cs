a4219e2ab54ca5eea82d0f4577294e2c40ef7ad1
Daniel Tierney <daniel.tierney1@icloud.com>
2024-11-15 18:04:24 +0000

    Project Generated
    The initial project files generated by Rider software ready for the forthcoming project.

---
+++ csvgenerator/csvgenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace csvgenerator;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        
+        // This application will be used to generate .csv files full of data.
+        // This data will be created to a user specification by using a selection menu
+    }
+}
