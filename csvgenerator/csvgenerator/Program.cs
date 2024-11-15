550759242cbb5a5b8f74e5ac6cd58c035140963b
Daniel Tierney <daniel.tierney1@icloud.com>
2024-11-15 18:35:41 +0000

    Refactored current code to start in while loop
    All current code moved into a while loop that should end on first input from user, or repeat on failed input

--- csvgenerator/csvgenerator/Program.cs
+++ csvgenerator/csvgenerator/Program.cs
@@ -4,29 +4,37 @@ class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
         
-        // This application will be used to generate .csv files full of data.
-        // This data will be created to a user specification by using a selection menu
+        bool newDataset = false;
+
+        while (!newDataset)
+        {
+            Console.WriteLine("Hello, World!");
         
-        // Opening console output to explain the program
-        Console.WriteLine("Welcome to CSV generator!\n" +
-                          "This application will help you to generate new datasets in the .cvs file format.\n" +
-                          "To begin, please confirm that you wish to create a new dataset\n");
+            // This application will be used to generate .csv files full of data.
+            // This data will be created to a user specification by using a selection menu
+        
+            // Opening console output to explain the program
+            Console.WriteLine("Welcome to CSV generator!\n" +
+                              "This application will help you to generate new datasets in the .cvs file format.\n" +
+                              "To begin, please confirm that you wish to create a new dataset ( Y / N )\n");
 
-        string userInput = "userinput";
-        try
-        {
-            userInput = Console.ReadLine();
-        }
-        catch (exception e)
-        {
-            Console.WriteLine(e.Message as string);
-        }
+            string userInput = "userinput";
+        
+            try
+            {
+                userInput = Console.ReadLine();
+            }
+            catch (exception e)
+            {
+                Console.WriteLine(e.Message as string);
+            }
 
-        if (userInput != "userinput")
-        {
-            
+            if (userInput == "userinput") continue;
+            if (userInput is "Y" or "y")
+            {
+                newDataset = true;
+            }
         }
 
         
