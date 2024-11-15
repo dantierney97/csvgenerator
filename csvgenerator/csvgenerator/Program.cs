aa20d752a8404917ce4889b46f221faac6b75b4f
Daniel Tierney <daniel.tierney1@icloud.com>
2024-11-15 18:08:37 +0000

    Intro message created
    Introduction to the application for the user.

--- csvgenerator/csvgenerator/Program.cs
+++ csvgenerator/csvgenerator/Program.cs
@@ -8,5 +8,11 @@ class Program
         
         // This application will be used to generate .csv files full of data.
         // This data will be created to a user specification by using a selection menu
+        
+        // Opening console output to explain the program
+        Console.WriteLine("Welcome to CSV generator!\n" +
+                          "This application will help you to generate new datasets in the .cvs file format.\n");
+        
+        
     }
 }
