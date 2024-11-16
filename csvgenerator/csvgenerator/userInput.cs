bc97b646e3b7bd8ae4acf410e234e57a5a279884
Daniel Tierney <daniel.tierney1@icloud.com>
2024-11-16 09:29:48 +0000

    Updated UserInput
    Updated UserInput method to accept user input and return it as generic type <T>. This allows for any datatype to be returned from the method.

--- csvgenerator/csvgenerator/userInput.cs
+++ csvgenerator/csvgenerator/userInput.cs
@@ -2,25 +2,33 @@ namespace csvgenerator;
 
 public class Userinput
 {
-    public string UserInput()
+    public T UserInput<T>()
     {
-        string input = null;
+        T input = default; // Initialise with default value for T
 
-        while (input is null)
+        while (true)
         {
             try
             {
-                input = Console.ReadLine();
+                string userInput = Console.ReadLine()!; // Null suppressed as exceptions are handled
+            
+                if (typeof(T) == typeof(string))
+                {
+                    // Directly cast to T if T is string
+                    return (T)(object)userInput!;
+                } // End if
+
+                // Convert the string input to type T
+                input = (T)Convert.ChangeType(userInput, typeof(T));
+                break;
             } // End Try
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-                Console.WriteLine(e.StackTrace);
                 Console.WriteLine("Please try again.");
             } // End Catch
-
-        } // End While
+        } // End While Loop
 
         return input;
-    }
+    } // End UserInput
 }
