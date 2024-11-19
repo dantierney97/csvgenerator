576dbb0aa5bc0d54276e63f937178757a8783606
Daniel Tierney <daniel.tierney1@icloud.com>
2024-11-19 10:32:22 +0000

    DebugLog class created
    Created a class to explore implementing debug output capabilities within the application

---
+++ csvgenerator/csvgenerator/DebugLog.cs
@@ -0,0 +1,17 @@
+namespace csvgenerator;
+
+public class DebugLog
+{
+    
+    // Constructor
+    public DebugLog()
+    {
+        Console.WriteLine("Debug Log created.");
+    } // End of Constructor
+
+    // Deconstructor
+    ~DebugLog()
+    {
+        Console.WriteLine("Debug Log destroyed.");
+    } // End of Deconstructor
+} // End of DebugLog class
